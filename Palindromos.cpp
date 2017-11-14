93d06313e1953885081d7218e0d2daa03c14d74e
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2017-11-14 10:57:34 -0400

    Create Palindromos.cpp

---
+++ Palindromos.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+char a[1001];
+int main(){
+    scanf("%s", a);
+    for(int i=0,j=strlen(a)-1;i<strlen(a);++i,--j)if(a[i]!=a[j]){printf("No");return 0;}
+    printf("Si");
+}
